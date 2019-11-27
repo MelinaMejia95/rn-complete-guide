8404a7e868a83e4bd314ba081e4fe2379a0e72c4
melinamejia95 <melinamejia95@gmail.com>
2019-11-27 10:28:51 -0500

    State handler to show items

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnterGoal] = useState('');
+  const [courseGoals, setCourseGoal] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnterGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoal(currentGoals => [...currentGoals, { 
+      id: Math.random().toString(), 
+      value: enteredGoal
+    }]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' style={styles.input} />
-        <Button title='ADD' />
+        <TextInput
+          placeholder='Course Goal'
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title='ADD' onPress={addGoalHandler} />
       </View>
-      <View>
-
-      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -29,5 +61,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
